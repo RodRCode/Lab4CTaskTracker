8ec6fce410e376cb72d44214f170e7a7d7174194
Rod' Rodriguez <Rod.R.Code@gmail.com>
2020-07-23 11:17:15 -0500

    first commit

---
+++ Lab4CTaskTracker/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * You have been hired by a firm to provide a console-based prototype of a task tracking application. Based on product owner requirements, spend the next few days implementing a task tracking application.
+ * */
+using System;
+
+namespace Lab4CTaskTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
