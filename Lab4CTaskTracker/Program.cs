52638505659a19c2b588bf525e07ecf80b5c257b
Rod' Rodriguez <Rod.R.Code@gmail.com>
2020-07-23 11:23:34 -0500

    Entered in additional text on what I need to do

--- Lab4CTaskTracker/Program.cs
+++ Lab4CTaskTracker/Program.cs
@@ -1,5 +1,17 @@
 ﻿/*
  * You have been hired by a firm to provide a console-based prototype of a task tracking application. Based on product owner requirements, spend the next few days implementing a task tracking application.
+   
+Things to do so far:
+    Need to figure out how to manipulate highlighted text on the console
+    (stretch) get string commands for up arrows, down arrows and such.
+    Do I need to rewrite the console text every time?
+    highlight text
+    strike through text
+    read and write to data file
+    How to keep track of data file and if an item has been actioned or deleted or pushed off to the future
+    move between pages on the screen (15 items at a go)
+    I am thinking the pointer to use is the one that has a pointer at the head and tail of the string to go to the next and previous items
+
  * */
 using System;
 
